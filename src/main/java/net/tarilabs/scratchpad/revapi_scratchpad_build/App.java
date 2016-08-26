3ee84ce1f90544ac7cd6945163a19bf7ff32fb02
tarilabs <matteo.mortari@gmail.com>
2016-08-26 08:31:10 +0200

    v1.0.0

---
+++ src/main/java/net/tarilabs/scratchpad/revapi_scratchpad_build/App.java
@@ -0,0 +1,13 @@
+package net.tarilabs.scratchpad.revapi_scratchpad_build;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
